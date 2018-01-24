23cff992f9e59d32a205f238d42b81a79dde0e36
Mohammad Niaz Murshed <fagun@Mohammads-MacBook-Air.local>
2018-01-24 00:13:30 -0500

    first commit

--- test_jacobi.m
+++
@@ -1,8 +0,0 @@
-syms x
-fplot(jacobiP(10,2,2,x))
-axis([-1 1 -7 7])
-grid on
-
-ylabel('P_n^{(\alpha,\beta)}(x)')
-% title('Zeros of Jacobi polynomials of degree=1,2,3 with a=3 and b=3');
-% legend('1','2','3','Location','best')
